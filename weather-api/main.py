77b999b0274497483328f7dad5a17af9436d1ece
Wiredu Clement <120313741+wireduclement@users.noreply.github.com>
2024-09-16 11:38:38 +0000

    update main.py

--- weather-api/main.py
+++ weather-api/main.py
@@ -12,6 +12,11 @@ load_dotenv()
 API_KEY = os.getenv("WEATHER_API_KEY")
 
 
+@app.get("/")
+def intro():
+   return {"message": "Please head over to the '/docs'"}
+
+
 @app.get("/weather/{city}")
 def weather_info(city: str):
    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
